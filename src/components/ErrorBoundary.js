53fe5bb9927e081316595834ab374a4402786a91
alina-bilanenko <Linyska007@gmail.com>
2018-04-26 22:18:08 +0300

    litle fixed

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -6,7 +6,7 @@ class ErrorBoundary extends Component {
         this.state = { hasError: false };
     }
 
-    componentDidCatch(error, info) {
+    componentDidCatch() {
         this.setState({ hasError: true });
     }
 
