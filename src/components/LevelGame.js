be0673ae5fe85c5d99eb53e16db9ae590b4dcaf7
alina-bilanenko <Linyska007@gmail.com>
2018-05-03 11:20:39 +0300

    add eslintrc

--- src/components/LevelGame.js
+++ src/components/LevelGame.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 
 class LevelGame extends  Component {
-    constructor(props) {
-        super(props);
-        this.selectOnChange = this.selectOnChange.bind(this);
-    }
 
-    selectOnChange(event) {
-        this.props.changeLevel(event.target.value);
+    selectOnChange = (e)=>{
+        this.props.changeLevel(e.target.value);
     }
 
     render () {
         return (
-            <div className = 'level'>
-                <select onChange = {this.selectOnChange}
-                        value = {this.props.currentLevel}
-                        className = "level-game"
-                        disabled = {this.props.disabledSelect}>
-                    <option value = "easy">Легкий</option>
-                    <option value = "medium">Средний</option>
-                    <option value = "hard">Сложный</option>
-                    <option value = "together">Играть с другом</option>
+            <div className='level'>
+                <select onChange={this.selectOnChange}
+                        value={this.props.currentLevel}
+                        className='level-game'
+                        disabled={this.props.disabledSelect}>
+                    <option value='easy'>Легкий</option>
+                    <option value='medium'>Средний</option>
+                    <option value='hard'>Сложный</option>
+                    <option value='together'>Играть с другом</option>
                 </select>
             </div>
         );
